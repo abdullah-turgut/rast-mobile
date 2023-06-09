19a6106b8d5896c47a81f51f6bf2888e1f181a69
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-06-09 16:47:13 +0300

    mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,65 @@
+//Brand-Logo
 import logo from '../assets/logo.png';
 
+//React-Icons
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { IoMdClose } from 'react-icons/io';
+
+//Social-Icons
+import { BsYoutube, BsInstagram, BsBehance, BsLinkedin } from 'react-icons/bs';
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <img src={logo} alt="" />
+      <a href="/">
+        <img src={logo} alt="rast-mobile-logo" className="logo" />
+      </a>
 
+      {/* When menu toggled on overlay on the left side */}
+      <div className="navbar-overlay"></div>
+
+      <HiMenuAlt4 size={40} className="burger-menu" />
       {/* Router Links */}
-      <ul className="nav-links">
-        <li>
-          <a href="/hakkimizda">Hakkımızda</a>
-        </li>
-        <li>
-          <a href="/juri-yarisma-yazilimi">Jüri - Yarışma Yazılımı</a>
-        </li>
-        <li>
-          <a href="/word-ninja">Word Ninja</a>
-        </li>
-        <li>
-          <a href="/word-pyramids">Word Pyramids</a>
-        </li>
-      </ul>
+      <div className="nav-links">
+        <IoMdClose size={30} />
+        <ul className="nav-route-links">
+          <li>
+            <a href="/hakkimizda">Hakkımızda</a>
+          </li>
+          <li>
+            <a href="/juri-yarisma-yazilimi">Jüri - Yarışma Yazılımı</a>
+          </li>
+          <li>
+            <a href="/word-ninja">Word Ninja</a>
+          </li>
+          <li>
+            <a href="/word-pyramids">Word Pyramids</a>
+          </li>
+        </ul>
 
-      {/* Social Links */}
-      <ul className="nav-social">
-        <li>
-          <a href="">Youtube</a>
-        </li>
-        <li>
-          <a href="">Instagram</a>
-        </li>
-        <li>
-          <a href="">Behance</a>
-        </li>
-        <li>
-          <a href="">Linked.in</a>
-        </li>
-      </ul>
+        {/* Social Links */}
+        <ul className="nav-social-links">
+          <li>
+            <a href="">
+              <BsYoutube size={20} />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <BsInstagram size={20} />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <BsBehance size={20} />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <BsLinkedin size={20} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
